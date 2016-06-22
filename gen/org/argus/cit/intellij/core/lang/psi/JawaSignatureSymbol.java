7e0be91c6d840df315a1b4a7351545238a2fb57d
Fengguo Wei <wfg611004900521@gmail.com>
2016-06-22 02:51:54 -0400

    add more files

---
+++ gen/org/argus/cit/intellij/core/lang/psi/JawaSignatureSymbol.java
@@ -0,0 +1,16 @@
+// This is a generated file. Not intended for manual editing.
+package org.argus.cit.intellij.core.lang.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+import org.argus.jawa.core.Signature;
+
+public interface JawaSignatureSymbol extends PsiElement {
+
+  @NotNull
+  PsiElement getApostropheId();
+
+  Signature getSignature();
+
+}
