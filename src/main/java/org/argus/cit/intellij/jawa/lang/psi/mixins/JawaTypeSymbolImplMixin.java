9d508078e8d83a6592cd0f5767339517a1beb0c0
Fengguo Wei <wfg611004900521@gmail.com>
2016-08-24 17:29:18 -0400

    update

--- src/main/java/org/argus/cit/intellij/jawa/lang/psi/mixins/JawaTypeSymbolImplMixin.java
+++ src/main/java/org/argus/cit/intellij/jawa/lang/psi/mixins/JawaTypeSymbolImplMixin.java
@@ -12,8 +12,10 @@ package org.argus.cit.intellij.jawa.lang.psi.mixins;
 
 
 import com.intellij.lang.ASTNode;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.*;
 import com.intellij.psi.codeStyle.JavaCodeStyleManager;
 import com.intellij.psi.codeStyle.JavaCodeStyleSettingsFacade;
@@ -21,13 +23,21 @@ import com.intellij.psi.filters.ElementFilter;
 import com.intellij.psi.filters.OrFilter;
 import com.intellij.psi.impl.CheckUtil;
 import com.intellij.psi.impl.PsiImplUtil;
+import com.intellij.psi.impl.source.PsiJavaCodeReferenceElementImpl;
+import com.intellij.psi.impl.source.resolve.ClassResolverProcessor;
+import com.intellij.psi.impl.source.resolve.JavaResolveUtil;
+import com.intellij.psi.impl.source.resolve.ResolveCache;
+import com.intellij.psi.impl.source.resolve.VariableResolverProcessor;
 import com.intellij.psi.impl.source.tree.JavaSourceUtil;
-import com.intellij.psi.impl.source.tree.java.PsiReferenceExpressionImpl;
+import com.intellij.psi.impl.source.tree.TreeElement;
+import com.intellij.psi.infos.CandidateInfo;
 import com.intellij.psi.scope.ElementClassFilter;
 import com.intellij.psi.scope.PsiScopeProcessor;
 import com.intellij.psi.scope.processor.FilterScopeProcessor;
 import com.intellij.psi.scope.util.PsiScopesUtil;
 import com.intellij.psi.stubs.IStubElementType;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtil;
 import com.intellij.util.IncorrectOperationException;
 import org.argus.cit.intellij.jawa.lang.psi.JawaStubBasedPsiElementBase;
 import org.argus.cit.intellij.jawa.lang.psi.JawaTypeSymbol;
@@ -44,6 +54,7 @@ import org.jetbrains.annotations.Nullable;
 public abstract class JawaTypeSymbolImplMixin
         extends JawaStubBasedPsiElementBase<JawaTypeSymbolStub>
         implements JawaTypeSymbol {
+    private static final Logger LOG = Logger.getInstance("#org.argus.cit.intellij.jawa.lang.psi.mixins.JawaTypeSymbolImplMixin");
 
     public JawaTypeSymbolImplMixin(@NotNull JawaTypeSymbolStub stub, @NotNull IStubElementType nodeType) {
         super(stub, nodeType);
@@ -102,7 +113,7 @@ public abstract class JawaTypeSymbolImplMixin
     @NotNull
     @Override
     public JavaResolveResult[] multiResolve(boolean incompleteCode) {
-        return PsiImplUtil.multiResolveImpl(this, incompleteCode, PsiReferenceExpressionImpl.OurGenericsResolver.INSTANCE);
+        return PsiImplUtil.multiResolveImpl(this, incompleteCode, JawaTypeSymbolImplMixin.OurGenericsResolver.INSTANCE);
     }
 
     @Nullable
@@ -135,7 +146,9 @@ public abstract class JawaTypeSymbolImplMixin
 
     @Override
     public TextRange getRangeInElement() {
-        return getApostropheId().getTextRange();
+        TreeElement nameChild = (TreeElement)getApostropheId();
+        int startOffset = nameChild.getStartOffsetInParent();
+        return new TextRange(startOffset, startOffset + nameChild.getTextLength());
     }
 
     @Nullable
@@ -151,7 +164,8 @@ public abstract class JawaTypeSymbolImplMixin
     }
 
     private String getNormalizedText() {
-        return JavaSourceUtil.getReferenceText(this);
+        String refText = JavaSourceUtil.getReferenceText(this);
+        return refText.replaceAll("`", "");
     }
 
     @Override
@@ -243,6 +257,11 @@ public abstract class JawaTypeSymbolImplMixin
     }
 
     @Override
+    public PsiReference getReference() {
+        return this;
+    }
+
+    @Override
     public boolean isSoft() {
         return false;
     }
@@ -255,5 +274,136 @@ public abstract class JawaTypeSymbolImplMixin
     @Override
     public PsiElement nameId() {
         return getApostropheId();
+    }
+
+    private PsiSubstitutor updateSubstitutor(PsiSubstitutor subst, PsiClass psiClass) {
+        PsiType[] parameters = this.getTypeParameters();
+        if(psiClass != null) {
+            subst = subst.putAll(psiClass, parameters);
+        }
+
+        return subst;
+    }
+
+    @NotNull
+    private JavaResolveResult[] resolve(final int kind, @NotNull PsiFile containingFile) {
+        switch (kind) {
+            case PsiJavaCodeReferenceElementImpl.CLASS_FQ_NAME_KIND: {
+                String text = getNormalizedText();
+                if (StringUtil.isEmptyOrSpaces(text)) return JavaResolveResult.EMPTY_ARRAY;
+
+                PsiClass aClass = JavaPsiFacade.getInstance(containingFile.getProject()).findClass(text, getResolveScope());
+                if (aClass == null) return JavaResolveResult.EMPTY_ARRAY;
+
+                if (!isQualified() && text.equals(aClass.getQualifiedName())) {
+                    if (containingFile instanceof PsiJavaFile && !((PsiJavaFile)containingFile).getPackageName().isEmpty()) {
+                        // classes in default (unnamed) package cannot be referenced from other packages
+                        return JavaResolveResult.EMPTY_ARRAY;
+                    }
+                }
+
+                return new JavaResolveResult[]{new CandidateInfo(aClass, updateSubstitutor(PsiSubstitutor.EMPTY, aClass), this, false)};
+            }
+            case PsiJavaCodeReferenceElementImpl.CLASS_IN_QUALIFIED_NEW_KIND: {
+                PsiElement parent = getParent();
+                final PsiExpression qualifier;
+                if (parent instanceof PsiNewExpression) {
+                    qualifier = ((PsiNewExpression)parent).getQualifier();
+                    LOG.assertTrue(qualifier != null);
+                }
+                else if (parent instanceof PsiJavaCodeReferenceElement) {
+                    return JavaResolveResult.EMPTY_ARRAY;
+                }
+                else {
+                    LOG.error("Invalid java reference!");
+                    return JavaResolveResult.EMPTY_ARRAY;
+                }
+
+                final PsiType qualifierType = qualifier.getType();
+                if (qualifierType == null) return JavaResolveResult.EMPTY_ARRAY;
+                if (!(qualifierType instanceof PsiClassType)) return JavaResolveResult.EMPTY_ARRAY;
+                final JavaResolveResult result = PsiUtil.resolveGenericsClassInType(qualifierType);
+                final PsiElement resultElement = result.getElement();
+                if (resultElement == null) return JavaResolveResult.EMPTY_ARRAY;
+                final PsiElement classNameElement = getReferenceNameElement();
+                if (!(classNameElement instanceof PsiIdentifier)) return JavaResolveResult.EMPTY_ARRAY;
+                final String className = classNameElement.getText();
+
+                final ClassResolverProcessor processor = new ClassResolverProcessor(className, this, containingFile);
+                resultElement.processDeclarations(processor, ResolveState.initial().put(PsiSubstitutor.KEY, result.getSubstitutor()), this, this);
+                return processor.getResult();
+            }
+            case PsiJavaCodeReferenceElementImpl.CLASS_NAME_KIND: {
+                final PsiElement classNameElement = getReferenceNameElement();
+                if (!(classNameElement instanceof PsiIdentifier)) return JavaResolveResult.EMPTY_ARRAY;
+                final String className = classNameElement.getText();
+                final ClassResolverProcessor processor = new ClassResolverProcessor(className, this, containingFile);
+                PsiScopesUtil.resolveAndWalk(processor, this, null);
+                return processor.getResult();
+            }
+            case PsiJavaCodeReferenceElementImpl.PACKAGE_NAME_KIND: {
+                String packageName = getNormalizedText();
+                Project project = getManager().getProject();
+                PsiPackage aPackage = JavaPsiFacade.getInstance(project).findPackage(packageName);
+                if (aPackage == null || !aPackage.isValid()) {
+                    return JavaPsiFacade.getInstance(project).isPartOfPackagePrefix(packageName) ?
+                            CandidateInfo.RESOLVE_RESULT_FOR_PACKAGE_PREFIX_PACKAGE : JavaResolveResult.EMPTY_ARRAY;
+                }
+                return new JavaResolveResult[]{new CandidateInfo(aPackage, PsiSubstitutor.EMPTY)};
+            }
+            case PsiJavaCodeReferenceElementImpl.CLASS_FQ_OR_PACKAGE_NAME_KIND:
+            case PsiJavaCodeReferenceElementImpl.CLASS_OR_PACKAGE_NAME_KIND: {
+                int classKind = kind == PsiJavaCodeReferenceElementImpl.CLASS_OR_PACKAGE_NAME_KIND ? PsiJavaCodeReferenceElementImpl.CLASS_NAME_KIND : PsiJavaCodeReferenceElementImpl.CLASS_FQ_NAME_KIND;
+
+                //A single-type-import declaration d in a compilation unit c of package p that imports a type named n shadows, throughout c, the declarations of:
+                //any top level type named n declared in another compilation unit of p
+                if (PsiTreeUtil.getParentOfType(this, PsiImportStatementBase.class) != null) {
+                    JavaResolveResult[] result = resolve(PsiJavaCodeReferenceElementImpl.PACKAGE_NAME_KIND, containingFile);
+                    return result.length == 0 ? resolve(classKind, containingFile) : result;
+                }
+
+                JavaResolveResult[] result = resolve(classKind, containingFile);
+
+                if (result.length == 1 && !result[0].isAccessible()) {
+                    JavaResolveResult[] packageResult = resolve(PsiJavaCodeReferenceElementImpl.PACKAGE_NAME_KIND, containingFile);
+                    if (packageResult.length != 0) {
+                        result = packageResult;
+                    }
+                }
+                else if (result.length == 0) {
+                    result = resolve(PsiJavaCodeReferenceElementImpl.PACKAGE_NAME_KIND, containingFile);
+                }
+
+                return result;
+            }
+        }
+
+        LOG.error(this);
+        return JavaResolveResult.EMPTY_ARRAY;
+    }
+
+    private static final class OurGenericsResolver implements ResolveCache.PolyVariantContextResolver<PsiJavaReference> {
+        private static final JawaTypeSymbolImplMixin.OurGenericsResolver INSTANCE = new JawaTypeSymbolImplMixin.OurGenericsResolver();
+
+        private OurGenericsResolver() {
+        }
+
+        @NotNull
+        public ResolveResult[] resolve(@NotNull PsiJavaReference ref, @NotNull PsiFile containingFile, boolean incompleteCode) {
+            JawaTypeSymbolImplMixin referenceElement = (JawaTypeSymbolImplMixin)ref;
+            int kind = PsiJavaCodeReferenceElementImpl.CLASS_FQ_NAME_KIND;
+            JavaResolveResult[] result = referenceElement.resolve(kind, containingFile);
+            if(incompleteCode && result.length == 0 && kind != 4 && kind != 5) {
+                VariableResolverProcessor processor = new VariableResolverProcessor(referenceElement, containingFile);
+                PsiScopesUtil.resolveAndWalk(processor, referenceElement, (PsiElement)null, true);
+                result = processor.getResult();
+                if(result.length == 0 && kind == 1) {
+                    result = referenceElement.resolve(2, containingFile);
+                }
+            }
+
+            JavaResolveUtil.substituteResults(referenceElement, result);
+            return result;
+        }
     }
 }
